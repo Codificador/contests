cbd8987ebd2f8e7e2ada151cf3e01c269e66565b
Fernando Lopes <fernando.mflopes87@gmail.com>
2019-02-06 21:44:22 -0200

    +

---
+++ uri/beginner/1041_Coordenadas_de_um_Ponto/main.py
@@ -0,0 +1,21 @@
+import sys
+from decimal import Decimal
+
+x, y = [Decimal(x) for x in sys.stdin.readline().split()]
+
+if x == 0 and y == 0:
+    print("Origem")
+elif x == 0:
+    print("Eixo Y")
+elif y == 0:
+    print("Eixo X")
+elif x > 0:
+    if y > 0:
+        print("Q1")
+    else:
+        print("Q4")
+else:
+    if y >= 0:
+        print("Q2")
+    else:
+        print("Q3")
